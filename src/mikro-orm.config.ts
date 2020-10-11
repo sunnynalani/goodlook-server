ff2a4b2296e18cb6c21519f73616ea4c5cf43cc8
jjchung3 <joejoonchung@gmail.com>
2020-10-10 17:37:05 -0700

    chore: hide db credentials...

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,9 @@ import { Client } from './entities/Client'
 import { __prod__ } from './constants'
 import { MikroORM } from '@mikro-orm/core'
 import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default {
   migrations: {
@@ -10,8 +13,8 @@ export default {
   },
   entities: [Client],
   dbName: 'goodlook',
-  user: 'postgres',
-  password: '091098',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   type: 'postgresql',
   debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0]
